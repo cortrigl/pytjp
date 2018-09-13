4649c452fa1257949d72a2bfc80f2c8e48ed875c
Larry Cortright <cortrigl@erau.edu>
2018-09-13 14:41:12 -0400

    converting to a global database class to incorporate static system stats

--- database.py
+++ database.py
@@ -3,9 +3,12 @@
 import sqlite3
 
 
-class UserData(object):
+class DataBase(object):
     def __init__(self):
-        self._db = sqlite3.connect('tjp_users.db')
+        self._db = None
+
+    def createDB(self):
+        self._db = sqlite3.connect('tjp.db')
         self._db.row_factory = sqlite3.Row
 
         self._db.cursor().execute('''
@@ -24,6 +27,29 @@ class UserData(object):
                                   ''')
         self._db.commit()
 
+        self._db.cursor().execute('''
+                                  CREATE TABLE system(
+                                  id INTEGER PRIMARY KEY,
+                                  num_royal_flush INT,
+                                  num_straight_flush INT,
+                                  num_four_of_a_kind INT,
+                                  num_full_house INT,
+                                  num_flush INT,
+                                  num_straight INT,
+                                  num_three_of_a_kind INT,
+                                  num_two_pair INT,
+                                  num_one_pair INT,
+                                  num_games INT,
+                                  num_distinct_users INT
+                                  )
+                                  ''')
+        self._db.commit()
+
+
+class UserData(object):
+    def __init__(self):
+        pass
+
     def add(self, user):
         self._db.cursor().execute('''
                                   INSERT INTO user(id, name, bbs, sysop,
@@ -36,3 +62,8 @@ class UserData(object):
     def get(self, user):
         self._db.cursor().execute("SELECT * FROM user WHERE id=:id",
                                   {'id': user}).fetchone()
+
+
+class SystemData(object):
+    def __init__(self):
+        pass
