31fbd51ff09e3ae6704375ee2960972b392d0fbe
Larry Cortright <cortrigl@erau.edu>
2018-09-14 13:06:20 -0400

    adding card panel

--- card_element.py
+++ card_element.py
@@ -6,7 +6,14 @@ from windows import initWindow
 
 
 class CardElement(object):
-    def __init__(self, suit, value, suit_color):
+    def __init__(self, y, x, suit, value, suit_color, parent_win):
         self.suit = suit
         self.value = value
         self.suit_color = suit_color
+        self.cmap = ColorMap()
+        init_win = initWindow(None, None, self.cmap)
+        self.ce_win = init_win.create(
+            'card', 'white_card', None, None, parent_win, y, x)
+
+    def draw_panel(self):
+        pass
